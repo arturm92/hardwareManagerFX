a407772fc30aa6d228e7e0442fb718f0476f29ba
Artur <Artur@DESKTOP-C302AAA>
2016-04-20 16:23:42 +0200

    commit

---
+++ src/address/view/MenuBarController.java
@@ -0,0 +1,45 @@
+package address.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class MenuBarController extends BaseController {
+
+	@FXML
+	private AnchorPane menuBar;
+	@FXML
+	private Button userInfoButton;
+
+	public MenuBarController() {
+		// initialize();
+	}
+
+	public void initialize() {
+		menuBar.setDisable(true);
+	}
+
+	public AnchorPane getMenuBar() {
+		return menuBar;
+	}
+
+	public void setUserInBar() {
+		userInfoButton.setText("logged" + main.getWorker().getLastName());
+		userInfoButton.setDisable(true);
+	}
+	
+	@FXML
+	public void goToProfileOverview(){
+		main.showProfileOverview();
+	}
+	
+	@FXML
+	public void goToWorkersOverview(){
+		main.showWorkerOverview();
+	}
+	
+	@FXML
+	public void goToHardwareOverview(){
+		main.showHardwareOverview();
+	}
+}
---
+++ src/address/view/ProfileOverviewController.java
@@ -0,0 +1,12 @@
+package address.view;
+
+public class ProfileOverviewController extends BaseController {
+
+	public ProfileOverviewController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void initialize(){
+		
+	}
+}
